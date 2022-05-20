2a040faa85d7b3dc4d062f0f27f5db911691a7ea
MaxwellChrist <ribthulu@gmail.com>
2022-05-20 11:13:49 -0400

    completed get reqest tests for projects

--- api/project/router.js
+++ api/project/router.js
@@ -1,6 +1,16 @@
 // build your `/api/projects` router here
 const express = require('express')
-
+const Projects = require('./model')
 const router = express.Router()
+
+router.get('/', (req, res) => {
+    Projects.getAll()
+    .then(result => {
+        result.map(item => {
+            item.project_completed = Boolean(item.project_completed)
+        })
+        res.json(result)
+    })
+})
 
 module.exports = router
