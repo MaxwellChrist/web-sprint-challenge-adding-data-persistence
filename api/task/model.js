0de561892e8a4c821de2d4679d4702453200fe3f
MaxwellChrist <ribthulu@gmail.com>
2022-05-20 16:09:38 -0400

    completed tasks get request

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,18 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+function getAll() {
+    return db('tasks as t')
+    .select('t.*', 'p.project_name', 'p.project_description')
+    .join('projects as p', 't.project_id', 'p.project_id')
+}
+
+// function post(item) {
+//     return db('resources').insert(item)
+//     .then(result => {
+//         return db('resources'). where("resource_id", result).first()
+//     })
+// }
+
+module.exports = {
+    getAll
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,6 +1,23 @@
-// build your `/api/tasks` router here
 const express = require('express')
-
+const Tasks = require('./model')
+// const { checkResourceName } = require('./middleware')
 const router = express.Router()
+
+router.get('/', (req, res) => {
+    Tasks.getAll()
+    .then(result => {
+        result.map(item => {
+            item.task_completed = Boolean(item.task_completed)
+        })
+        res.json(result)
+    })
+})
+
+// router.post('/', checkResourceName, (req, res) => {
+//     Tasks.post(req.body)
+//     .then(result => {
+//         res.json(result)
+//     })
+// })
 
 module.exports = router
