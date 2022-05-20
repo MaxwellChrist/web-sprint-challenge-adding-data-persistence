46830306af57513ad4ad72b4d88bf572d8f6c3dc
MaxwellChrist <ribthulu@gmail.com>
2022-05-20 13:27:31 -0400

    completed resources get request

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,6 +1,13 @@
-// build your `/api/resources` router here
 const express = require('express')
+const Resources = require('./model')
 
 const router = express.Router()
+
+router.get('/', (req, res) => {
+    Resources.getAll()
+    .then(result => {
+        res.json(result)
+    })
+})
 
 module.exports = router
