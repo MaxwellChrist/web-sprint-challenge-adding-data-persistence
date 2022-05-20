a6ccbc535283e70528a37fc5df00e8f13fdd30ef
MaxwellChrist <ribthulu@gmail.com>
2022-05-20 09:53:04 -0400

    built server.js and connected it to project router, resource router, and task router

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+const projectsRouter = require('./project/router.js');
+const resourceRouter = require('./resource/router.js');
+const taskRouter = require('./task/router.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourceRouter);
+server.use('/api/tasks', taskRouter);
+
+server.use('*', (req, res) => {
+    res.json({ message: "Server up and running!" })
+})
+
+module.exports = server;
